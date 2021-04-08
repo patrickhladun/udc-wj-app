b57b4b81c237261268d760d2e068f2906608e9dc
Patrick <patrickhladun@gmail.com>
2021-04-08 16:53:50 +0100

    Send fetched weather data and form data to the backend

--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-
-
 // Create a new date instance dynamically
 // const d = new Date();
 // const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -17,6 +15,9 @@
 /* Function to GET Web API Data*/
 /* Function to POST data */
 /* Function to GET Project Data */
+// TODO: Add data to an object on server side
+// TODO: Fetch all entries from the backend
+// TODO: Display the entries on the front end
 
 const generate = document.getElementById('generate');
 
@@ -26,32 +27,32 @@ generate.addEventListener('click', async () => {
 
 const getWeather = () => {
     const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
     const appid = 'd7d5f6e358b6ae754ddaec7f01f9da0c';
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${appid}`;
-    
+
+    const form = {
+        zip,
+        feelings
+    }
+
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => addEntry({...data, ...form}))
     .catch(error => console.log(`Error: ${error}`));
 };
 
-const addEntry = () => {
-    // const payload = {
-    //     'zip': 'zip-code'
-    // };
-    // const resp = fetch('/entry', {
-    //     method: 'POST',
-    //     credentials: 'same-origin',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(payload)
-    // }) 
-    // try {
-    //     const response = resp.json();
-    //     return response;
-    // }
-    // catch(error) {
-    //     console.log(`Error: ${error}`);
-    // }
+const addEntry = (data) => {
+    const payload = data;
+    const resp = fetch('/entry', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+    }) 
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(`Error: ${error}`));
 };
 
 const getEntries = () => {
